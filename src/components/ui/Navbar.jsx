4636b78d95d59f3a3b348491cb046e3234c6d697
Sean Sipus <sean.sipus@gmail.com>
2023-03-20 14:10:13 -0400

    Remove unused useRef hook from import in navbar component

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.png';
 import category from '../../assets/Category.png';
