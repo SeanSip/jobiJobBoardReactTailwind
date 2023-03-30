c15c3c0cb84b82194cdc35859ca7461efbb851fa
Sean Sipus <sean.sipus@gmail.com>
2023-03-30 00:26:58 -0400

    [FR] Add skills feature to job post page
    - Add react select
    - Add Skills component that works with react select
    - Import Skills component into PostJobPage
    - Add test display of job skills onto job card
    component

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,1 +1,29 @@
-export const skills = ['JavaScript', 'React', 'CSS', 'HTML', 'Node.js'];
+import React, { useState } from 'react';
+import Select from 'react-select';
+
+const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' },
+];
+
+const Skills = ({ jobDetails, setJobDetails }) => {
+  const handleOptionSelect = selectedOption => {
+    setJobDetails(prevState => ({ ...prevState, skills: selectedOption }));
+  };
+
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  return (
+    <div className="App">
+      <Select
+        isMulti
+        defaultValue={jobDetails.skills}
+        onChange={handleOptionSelect}
+        options={options}
+      />
+    </div>
+  );
+};
+
+export default Skills;
